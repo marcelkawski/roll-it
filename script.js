01c247b485754a1b29fd21119047812e24d978d3
marcelkawski <marcel.kawski1@gmail.com>
2023-04-18 15:59:18 +0300

    Add alternative way of initalizing and resetting game state

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
 'use strict';
 
+// comment for the 2nd solution of initializing and reseting game state:
 let currentPlayer = 0;
 let scores = [0, 0];
 let currentScore = 0;
+let playing = true;
 
-const minWinnerScore = 10;
+const minWinnerScore = 100;
 const numPlayers = 2;
-
-// const defActPlayerBckgrndColor =
 
 const rollImg = document.querySelector('.dice');
 const player0Section = document.querySelector('.player--0');
 const player1Section = document.querySelector('.player--1');
+
+const updateCurrentScore = (player, currentScore) =>
+  (document.querySelector(`#current--${player}`).textContent = currentScore);
+
+const updateScore = (player, score) =>
+  (document.querySelector(`#score--${player}`).textContent = score);
+
+const resetScores = function (currentScore, scores) {
+  for (let i = 0; i < numPlayers; i++) {
+    updateCurrentScore(i, currentScore);
+    updateScore(i, scores[i]);
+  }
+};
+
+// ---------------------------------------------------------------------------------------------
+// We could also make initializing and resetting game state like this (In .btn--new event handler I check the current state and update it basing on the current state. We can omit that and just set the initial value and remove every additional class etc. to make it like at the beginning of the game. Then we need to comment the initialization of the variables above ^ and .btn--new event handler below and uncomment alternative event handler below.):
+
+// let currentPlayer, scores, currentScore, playing;
+
+// const initGame = function () {
+//   currentPlayer = 0;
+//   scores = [0, 0];
+//   currentScore = 0;
+//   playing = true;
+
+//   resetScores(currentScore, scores);
+
+//   player0Section.classList.remove('player--winner');
+//   player1Section.classList.remove('player--winner');
+//   player0Section.classList.add('player--active');
+//   player1Section.classList.remove('player--active');
+
+//   rollImg.classList.add('hidden');
+// };
+
+// initGame();
+
+// ---------------------------------------------------------------------------------------------
+
+const getNextPlayer = () => (currentPlayer + 1) % 2;
 
 const switchPlayer = function () {
   currentPlayer = getNextPlayer();
@@ -25,64 +65,46 @@ const switchPlayerUpdateCurrentScore = function () {
   switchPlayer();
 };
 
-const getNextPlayer = () => (currentPlayer + 1) % 2;
-
 const drawDice = () => Math.trunc(Math.random() * 6 + 1);
-
-const updateCurrentScore = function (player, currentScore) {
-  document.querySelector(`#current--${player}`).textContent = currentScore;
-};
-
-const updateScore = function (player, score) {
-  document.querySelector(`#score--${player}`).textContent = score;
-};
 
 const getCurrentPlayerSection = () =>
   document.querySelector(`.player--${currentPlayer}`);
 
-const markWinner = function () {
-  const curPlayerSection = getCurrentPlayerSection();
-  // const playersNames = document.querySelectorAll('.name');
-  // const playersScores = document.querySelectorAll('.score');
-  // const playersCurrents = document.querySelectorAll('.current');
-  curPlayerSection.classList.add('player--winner');
-  // playersNames[currentPlayer].classList.add('player--winner');
-  // playersScores[currentPlayer].classList.add('player--winner');
-  // playersCurrents[currentPlayer].classList.add('player--winner');
-};
+const markWinner = () =>
+  getCurrentPlayerSection().classList.add('player--winner');
 
 // rolling a dice
 document.querySelector('.btn--roll').addEventListener('click', function () {
-  const dice = drawDice();
-  rollImg.classList.remove('hidden');
-  rollImg.src = `resources/dice-${dice}.png`;
+  if (playing) {
+    const dice = drawDice();
+    rollImg.classList.remove('hidden');
+    rollImg.src = `resources/dice-${dice}.png`;
 
-  if (dice !== 1) {
-    currentScore += dice;
-    updateCurrentScore(currentPlayer, currentScore);
-  } else switchPlayerUpdateCurrentScore();
-});
-
-// holding a current score
-document.querySelector('.btn--hold').addEventListener('click', function () {
-  if (currentScore > 0) {
-    scores[currentPlayer] += currentScore;
-    updateScore(currentPlayer, scores[currentPlayer]);
-    if (scores[currentPlayer] >= minWinnerScore) {
-      markWinner();
+    if (dice !== 1) {
+      currentScore += dice;
+      updateCurrentScore(currentPlayer, currentScore);
     } else switchPlayerUpdateCurrentScore();
   }
 });
 
-const resetScores = function (currentScore, scores) {
-  for (let i = 0; i < numPlayers; i++) {
-    updateCurrentScore(i, currentScore);
-    updateScore(i, scores[i]);
+// holding a current score
+document.querySelector('.btn--hold').addEventListener('click', function () {
+  if (playing) {
+    if (currentScore > 0) {
+      scores[currentPlayer] += currentScore;
+      updateScore(currentPlayer, scores[currentPlayer]);
+      if (scores[currentPlayer] >= minWinnerScore) {
+        playing = false;
+        markWinner();
+        rollImg.classList.add('hidden');
+      } else switchPlayerUpdateCurrentScore();
+    }
   }
-};
+});
 
 // reseting game state
 document.querySelector('.btn--new').addEventListener('click', function () {
+  playing = true;
   const curPlayerSection = getCurrentPlayerSection();
   if (curPlayerSection.classList.contains('player--winner'))
     curPlayerSection.classList.remove('player--winner');
@@ -93,3 +115,6 @@ document.querySelector('.btn--new').addEventListener('click', function () {
 
   rollImg.classList.add('hidden');
 });
+
+// reseting game state (2nd solution)
+// document.querySelector('.btn--new').addEventListener('click', initGame);
